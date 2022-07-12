0e4fd29a236ada79dbb8c93ebab266c013b3290c
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-07-11 21:52:37 -0500

    added the variables files for both prod and dev environments

---
+++ dev/vars.tf
@@ -0,0 +1,26 @@
+#----dev/vars.tf---
+
+variable "vpc_cider" {
+    default = "19.168.0.0/16"
+}
+variable "tenancy" {
+    value = default
+}
+variable "subnet_cidr" {
+    value = "192.168.1.0/24"
+}
+
+variable "ec2_count" {
+    value = 1
+}
+
+variable "instance_type" {
+    value = "t2.micro"
+}
+
+variable "ami_id" {
+    value = ""
+}
+variable "region" {
+    default = "us-east-1"
+}
--- prod/main.tf
+++ prod/main.tf
@@ -1,20 +1,21 @@
-# ---PROD ENVIRO---
+# ---prod enviro---
+
 provider "aws" {
-    region = "us-east-1"
+    region = "${var.region}"
 }
 
 module "my_vpc" {
     source = "../modules/vpc"
-    vpc_cidr = "192.168.0.0/16"
+    vpc_cidr = "${var.vpc_cidr}"
     tenancy = default
     vpc_id = "${module.my_vpc.vpc_id}"
-    subnet_cidr = "192.168.1.0/24"
+    subnet_cidr = "${var.subnet_cidr}"
 }
 
 module "my_ec2" {
     source = "../module/ec2"
-    ec2_count = 1
-    ami_id = ""
-    instance_type = "t2.micro"
+    ec2_count = "${var.ec2_count}"
+    ami_id = "${var.ami_id}"
+    instance_type = "${var.instance_type}"
     subnet_id = "${module.my_vpc.subnet_id}"
 }
